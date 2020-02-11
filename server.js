5563d6ac96f18d1ded5775f91b25aad78e71b7dd
Tony Sulfaro <tony116523@gmail.com>
2020-02-11 14:26:00 -0500

    add some more attributes to object

--- server.js
+++ server.js
@@ -14,21 +14,24 @@ createShow({
   name: 'Hasbin Hotel',
   about: 'Hazbin Hotel is an American adult animated web series created, directed, written and produced by Vivienne "Vivziepop" Medrano. The official pilot was released on YouTube on October 28, 2019. On November 6, 2019, Medrano stated on Patreon that there was no stated release date for a second episode',
   airdate: 'October 28, 2019',
-  imgurl: '/images/space.jpg'
+  imgurl: '/images/hasbin.jpg',
+  route: '/hasbin-hotel'
 })
 
 createShow({
   name: 'Rick and Morty',
   about: 'After having been missing for nearly 20 years, Rick Sanchez suddenly arrives at daughter Beth\'s doorstep to move in with her and her family. Although Beth welcomes Rick into her home, her husband, Jerry, isn\'t as happy about the family reunion. Jerry is concerned about Rick, a sociopathic scientist, using the garage as his personal laboratory. In the lab, Rick works on a number of sci-fi gadgets, some of which could be considered dangerous. But that\'s not all Rick does that concerns Jerry. He also goes on adventures across the universe that often involve his grandchildren, Morty and Summer.',
   airdate: 'December 2, 2013',
-  imgurl: 'https://ib4.hulu.com/user/v3/artwork/d76d6361-3fbf-4842-8dd7-e05520557280?base_image_bucket_name=image_manager&base_image=557550d6-2916-4c92-88df-ab4ba4882821&region=US&format=jpeg&size=952x536'
+  imgurl: '/images/rick-morty.jpeg',
+  route: '/rick-and-morty'
 })
 
 createShow({
   name: 'Aggretsuko',
   about: 'Aggretsuko, also known by its original Japanese title Aggressive Retsuko, is a Japanese anime musical comedy franchise based on the eponymous character created by "Yeti" for the mascot company Sanrio.',
   airdate: 'April 20, 2018',
-  imgurl: '/images/space.jpg'
+  imgurl: '/images/aggretsuko.jpg',
+  route: '/aggretsuko'
 })
 
 express()
