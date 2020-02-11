7fe4f0cbde2ff4ab650de4041849805407feb4e7
Tony Sulfaro <tony116523@gmail.com>
2020-02-11 15:17:08 -0500

    move to generate show view per show

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ createShow({
   name: 'Hasbin Hotel',
   about: 'Hazbin Hotel is an American adult animated web series created, directed, written and produced by Vivienne "Vivziepop" Medrano. The official pilot was released on YouTube on October 28, 2019. On November 6, 2019, Medrano stated on Patreon that there was no stated release date for a second episode',
   airdate: 'October 28, 2019',
+  like: 'The animation itself is top-notch.',
+  dislike: 'The storyline itself is a bit much to follow and they introduce too many characters for a pilot episode but I found it humerous and ejoyable nonetheless',
   imgurl: '/images/hasbin.jpg',
   route: '/hasbin-hotel'
 })
@@ -22,6 +24,8 @@ createShow({
   name: 'Rick and Morty',
   about: 'After having been missing for nearly 20 years, Rick Sanchez suddenly arrives at daughter Beth\'s doorstep to move in with her and her family. Although Beth welcomes Rick into her home, her husband, Jerry, isn\'t as happy about the family reunion. Jerry is concerned about Rick, a sociopathic scientist, using the garage as his personal laboratory. In the lab, Rick works on a number of sci-fi gadgets, some of which could be considered dangerous. But that\'s not all Rick does that concerns Jerry. He also goes on adventures across the universe that often involve his grandchildren, Morty and Summer.',
   airdate: 'December 2, 2013',
+  like: 'The animation itself is top-notch. Each episode as well is stand alone for the mostpart.',
+  dislike: 'They need to release episodes more often so I can watch them.',
   imgurl: '/images/rick-morty.jpeg',
   route: '/rick-and-morty'
 })
@@ -30,6 +34,8 @@ createShow({
   name: 'Aggretsuko',
   about: 'Aggretsuko, also known by its original Japanese title Aggressive Retsuko, is a Japanese anime musical comedy franchise based on the eponymous character created by "Yeti" for the mascot company Sanrio.',
   airdate: 'April 20, 2018',
+  like: 'Just look at it. Have you ever seen anything cuter??',
+  dislike: 'They need to release episodes more often so I can watch them.',
   imgurl: '/images/aggretsuko.jpg',
   route: '/aggretsuko'
 })
@@ -40,6 +46,6 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index', { shows: shows }))
   .get('/hasbin-hotel', (req, res) => res.render('pages/show', { shows: shows[0] }))
-  .get('/rick-and-morty', (req, res) => res.render('pages/rick-and-morty', { show: shows }))
-  .get('/aggretsuko', (req, res) => res.render('pages/aggretsuko', { show: shows }))
+  .get('/rick-and-morty', (req, res) => res.render('pages/show', { shows: shows[1] }))
+  .get('/aggretsuko', (req, res) => res.render('pages/show', { shows: shows[2] }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
